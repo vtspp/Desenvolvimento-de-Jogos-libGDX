3eb9eb03ecd26205f6c8f7188d4d7b3a7c6b87e4
Victor <vtsppinho@gmail.com>
2021-01-09 22:51:38 -0300

    Adding the layer

--- core/src/br/com/vtspp/world/World.java
+++ core/src/br/com/vtspp/world/World.java
@@ -9,31 +9,52 @@ import br.com.vtspp.enums.Element;
 
 public class World {
 
-    public int[][][] map = new int[80][45][2];
+    private int[][][] map = new int[80][45][2];
 
     public void regenerate() {
         new Blocks().initializeRegister();
         for (int x = 0; x < getWidth(); x++) {
             for (int y = 0; y < getHeight(); y++) {
-                if (y < 10) {
-                    map[x][y][1] = Element.OBSIDIAN.ordinal();
-                }
-                else if (y < 20) {
-                    map[x][y][1] = Element.COBBLESTONE.ordinal();
-                }
-                else if (y < 22) {
-                    map[x][y][1] = Element.DIRTY.ordinal();
+                for (int l = 0; l < getLayer(); l++) {
+                    if (y < 10) {
+                        map[x][y][l] = Element.OBSIDIAN.ordinal();
+                    }
+                    else if (y < 20) {
+                        map[x][y][l] = Element.COBBLESTONE.ordinal();
+                    }
+                    else if (y < 22) {
+                        map[x][y][l] = Element.DIRTY.ordinal();
+                    }
                 }
             }
         }
     }
 
     public void render(Batch batch) {
+       renderBackground(batch);
+       renderForeground(batch);
+    }
+
+    private void renderForeground(Batch batch) {
         Texture texture = null;
 
         for (int x = 0; x < getWidth(); x++) {
             for (int y = 0; y < getHeight(); y++) {
-                texture = Blocks.getBlockById(map[x][y][1]).texture;
+                texture = getBlock(x, y, 1).texture;
+
+                if (texture != null) {
+                    batch.draw(texture, x * Block.TITLE_SIZE, y * Block.TITLE_SIZE);
+                }
+            }
+        }
+    }
+
+    private void renderBackground(Batch batch) {
+        Texture texture = null;
+
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                texture = getBlock(x, y, 0).texture;
 
                 if (texture != null) {
                     batch.draw(texture, x * Block.TITLE_SIZE, y * Block.TITLE_SIZE);
